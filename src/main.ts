7754743ec580444eb6a0db496da327973b7d201d
Stuart Kennedy <rav2040@gmail.com>
2023-04-12 10:32:25 +1200

    Add working-directory input

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { execFileSync } from "child_process";
+import { join } from "path";
 import { getInput, setOutput, setFailed } from "@actions/core";
 
 async function main() {
     try {
-        const name = getInput("name");
-        const cmd = getInput("cmd");
+        const name = getInput("name", { required: true });
+        const cmd = getInput("cmd", { required: true });
+        const workingDirectory = getInput("working-directory") || "./tf/applications/ubiquity";
         const [file, ...args] = cmd.split(/\s/g).filter(Boolean);
-        console.log(file, args)
-        const output = execFileSync(file, args, { encoding: "utf8" });
+        const output = execFileSync(file, args, {
+            cwd: join(process.cwd(), workingDirectory),
+            encoding: "utf8",
+        });
         setOutput(name, output);
     } catch (err) {
         if (err instanceof Error) setFailed(err);
