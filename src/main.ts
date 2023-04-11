b01b9939d1b67e1c5a8eee234eafc76b3ee23207
Stuart Kennedy <rav2040@gmail.com>
2023-04-12 09:12:40 +1200

    Update split regex

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ async function main() {
     try {
         const name = getInput("name");
         const cmd = getInput("cmd");
-        const [file, ...args] = cmd.split(/\s+/g);
+        const [file, ...args] = cmd.split(/\s+(?=([^"]*"[^"]*")*[^"]*$)/g);
+        console.log(file, args)
         const output = execFileSync(file, args, { encoding: "utf8" });
         setOutput(name, output);
     } catch (err) {
