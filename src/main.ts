804f4e6a6eddde04e737b92da06ba36cde72611a
Stuart Kennedy <rav2040@gmail.com>
2023-04-11 12:46:57 +1200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { execFileSync } from "child_process";
+import { getInput, setOutput, setFailed } from "@actions/core";
+
+const CMD_SPLIT_REGEX = /\s+(?=([^"]*"[^"]*")*[^"]*$)/g;
+
+async function main() {
+    try {
+        const name = getInput("name");
+        const cmd = getInput("cmd");
+        const [file, ...args] = cmd.split(CMD_SPLIT_REGEX);
+        const output = execFileSync(file, args);
+        setOutput(name, output);
+    } catch (err) {
+        if (err instanceof Error) setFailed(err);
+    }
+}
+
+main();
