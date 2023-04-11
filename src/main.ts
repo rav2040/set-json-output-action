0336fbfa5049b8177786689d8e2dbe4425183845
Stuart Kennedy <rav2040@gmail.com>
2023-04-11 13:13:12 +1200

    Update split regex

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { execFileSync } from "child_process";
 import { getInput, setOutput, setFailed } from "@actions/core";
 
-const CMD_SPLIT_REGEX = /\s+(?=([^"]*"[^"]*")*[^"]*$)/g;
-
 async function main() {
     try {
         const name = getInput("name");
         const cmd = getInput("cmd");
-        const [file, ...args] = cmd.split(CMD_SPLIT_REGEX);
+        const [file, ...args] = cmd.split(/\s+/g);
         const output = execFileSync(file, args, { encoding: "utf8" });
+        console.log(output);
         setOutput(name, output);
     } catch (err) {
         if (err instanceof Error) setFailed(err);
